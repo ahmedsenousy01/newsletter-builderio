bcdbfc736f79372a933c0acd386831a0f26612a3
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2024-10-02 12:57:42 +0300

    built newsletter distribution feature

--- src/app/api/newsletter/distribute/route.ts
+++ src/app/api/newsletter/distribute/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -18,23 +20,30 @@ export async function POST(request: NextRequest) {
   const { newValue, operation, modelName } = body;
 
   if (operation === "publish" && modelName === "newsletter-emails") {
+    // Get the subscribers from the Builder.io API
+    const res = await fetch(
+      `https://cdn.builder.io/api/v3/content/newsletter-subscription-list?apiKey=${env.NEXT_PUBLIC_BUILDER_API_KEY}`,
+    );
+    const content = await res.json();
+    const emails = content?.results?.map((item: any) => item?.data?.email);
+
     // Extract the necessary data from the payload
-    const emailContent = newValue.data.body; // Customize this based on your payload structure
-    const emailSubject = newValue?.subject ?? "New Newsletter"; // You can use the name of the newsletter or another field
+    const emailContent = newValue.data?.body;
+    const emailSubject = newValue.data?.subject;
 
     // Set up Nodemailer transport (you need to configure your email service)
     const transporter = nodemailer.createTransport({
-      service: "gmail", // or use another email provider
+      service: "gmail",
       auth: {
-        user: env.NODEMAILER_EMAIL ?? "liwangaller.dev@gmail.com", // your email
-        pass: env.NODEMAILER_PASSWORD ?? "Put real password here", // your email password or app-specific password
+        user: env.NODEMAILER_EMAIL,
+        pass: env.NODEMAILER_PASSWORD,
       },
     });
 
     // Set up the email options
     const mailOptions = {
-      from: env.NODEMAILER_EMAIL, // sender address
-      to: "ahmedsenousy.route@gmail.com", // list of receivers
+      from: `Liwan Gallery <${env.NODEMAILER_EMAIL}>`, // sender address
+      to: emails.length > 0 ? emails : ["ahmedsenousy.route@gmail.com"], // list of receivers
       subject: emailSubject, // Subject line
       text: emailContent, // plain text body
     };
@@ -42,10 +51,9 @@ export async function POST(request: NextRequest) {
     try {
       // Send the email
       await transporter.sendMail(mailOptions);
-      console.log("Email sent successfully!");
 
       // Respond to the webhook
-      return NextResponse.json({ status: "success", message: "Email sent" });
+      return NextResponse.json({ status: "success", message: "Emails sent" });
     } catch (error) {
       console.error("Error sending email:", error);
       return NextResponse.json({
