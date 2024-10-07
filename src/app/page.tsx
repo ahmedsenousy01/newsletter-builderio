f0611446bd264cd3fd1bab1b64f8714222837991
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2024-10-07 13:39:43 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
-export default function HomePage() {
-  const apiRoute = `https://api.instagram.com/oauth/authorize/?app_id=488837020617937&redirect_uri=https://newsletter-builderio.vercel.app/&response_type=code&scope=user_profile,user_media`;
+import { env } from "@/env";
+import { redirect } from "next/navigation";
+
+export default function HomePage({
+  searchParams,
+}: {
+  searchParams: { code?: string; access_token?: string };
+}) {
+  const apiRoute = `https://api.instagram.com/oauth/authorize/?app_id=${env.INSTAGRAM_APP_ID}&redirect_uri=${env.INSTAGRAM_REDIRECT_URI}&response_type=code&scope=user_profile,user_media`;
+
+  if (searchParams.code) {
+    redirect(
+      `https://api.instagram.com/oauth/access_token?app_id=${env.INSTAGRAM_APP_ID}&app_secret=${env.INSTAGRAM_APP_SECRET}grant_type=authorization_code&redirect_uri=${env.INSTAGRAM_REDIRECT_URI}&code=${searchParams.code}`,
+    );
+  }
+
+  if (searchParams.access_token) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        {searchParams.access_token}
+      </div>
+    );
+  }
 
   return <a href={apiRoute}>authorize</a>;
 }
+// AQAgzMy2bcUxqThKR_3Fvi-3jHumleRzK3Uv4SLn7PhBjk3GtNKl5OT4wO8NZbeXcon6b7J7SPYE9_5e0QGvnHAaZyWPs96FXH86no0w0XIgP47cLTQogj80NNOC1EVz7Kj6eakWi5_r8iiFvT8747CM7Z7VmpmEfkzPZOqt8JTbeQV31ktXUuabm17LYd1NowGSjb9TxE46OWXtFSnOX6GGg_B-GT3PHR8kjJgFfA6JSg
