62565c6947e7e9e93032897db0c35b6d573368b2
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2024-10-07 13:30:24 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function HomePage() {
-  const apiRoute = `https://api.instagram.com/oauth/authorize/?app_id=${process.env.INSTAGRAM_APP_ID}&redirect_uri=https://newsletter-builderio.vercel.app/&response_type=code&scope=user_profile,user_media`;
+  const apiRoute = `https://api.instagram.com/oauth/authorize/?app_id=488837020617937&redirect_uri=https://newsletter-builderio.vercel.app/&response_type=code&scope=user_profile,user_media`;
 
   return <a href={apiRoute}>authorize</a>;
 }
